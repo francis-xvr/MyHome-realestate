67d874daeda854e33ec362671d7606693288ece1
Francis-xav1er <francis.xav1er@neoito.com>
2024-12-29 22:23:54 +0530

    Build chores

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: 'MyHome_realestate',
+  base: 'MyHome-realestate',
   build: {
     outDir: './docs',
     rollupOptions: {
